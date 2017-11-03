a2a8763ad673c8b4041dff684717a464daf11129
manifest <riku.torenius@gmail.com>
2017-11-04 00:49:39 +0200

    now uses electrum nodes instead of scraping a website, single goroutine per node.

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # BTCLotto
 
-Inspired by [saracens](https://github.com/saracen) [directory.io](http://directory.io), this script generates (random, not sequenced) private keys using BTCSuite's utilities and goquery for checking the keys for prior use.
+Inspired by [saracens](https://github.com/saracen) [directory.io](http://directory.io), this script generates (random, not sequenced) private keys using BTCSuite's utilities and go-electrum to check the
+given keys against nodes.
 
-Once it finds a key with prior transactions or any balance (check done through [blockchain.info](https://blockchain.info)), it writes the private and public key into needles.txt in the application root.
+Each node is given it's own goroutine to check the keys with, the more nodes you bother the quicker it should work, in theory. Addresses are given in the electrum.txt file, every row contains one node-address in the format address:port.
+
 
 ## Installing
 ```
--- main.go
+++ main.go
@@ -1,63 +1,82 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"math/big"
 	"math/rand"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcutil"
-
-	"github.com/PuerkitoBio/goquery"
+	"github.com/d4l3k/go-electrum/electrum"
 )
 
 const privatekeyConstant = "115792089237316195423570985008687907853269984665640564039457584007913129639936"
 
 func main() {
-
+	var wg sync.WaitGroup
 	// rng from unix time
 	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
 	// const string to big.Int
 	maxprivatekeys := new(big.Int)
 	maxprivatekeys.SetString(privatekeyConstant, 10)
-	for {
-		// random number from 0 to privatekeyConstant
-		pktocheck := new(big.Int)
-		pktocheck.Rand(rng, maxprivatekeys)
 
-		pktodisplay := make([]byte, 32)
+	file, err := os.Open("electrum.txt")
+	if err != nil {
+		fmt.Printf("%s\n", err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	var nodes []string
+	for scanner.Scan() {
+		nodes = append(nodes, scanner.Text())
+	}
+	wg.Add(len(nodes))
+	for _, line := range nodes {
+		go func(address string, rng *rand.Rand, maxprivatekeys *big.Int) {
+			defer wg.Done()
+			node := electrum.NewNode()
+			if err := node.ConnectTCP(address); err != nil {
+				log.Fatal(err)
+			}
+			for {
+				pktocheck := new(big.Int)
+				pktocheck.Rand(rng, maxprivatekeys)
 
-		copy(pktodisplay[32-len(pktocheck.Bytes()):], pktocheck.Bytes())
+				pktodisplay := make([]byte, 32)
 
-		_, public := btcec.PrivKeyFromBytes(btcec.S256(), pktodisplay)
+				copy(pktodisplay[32-len(pktocheck.Bytes()):], pktocheck.Bytes())
 
-		// caddr, _ := btcutil.NewAddressPubKey(public.SerializeCompressed(), &chaincfg.MainNetParams)
-		uaddr, _ := btcutil.NewAddressPubKey(public.SerializeUncompressed(), &chaincfg.MainNetParams)
+				_, public := btcec.PrivKeyFromBytes(btcec.S256(), pktodisplay)
 
-		doc, err := goquery.NewDocument("https://blockchain.info/address/" + uaddr.EncodeAddress())
-		if err != nil {
-			fmt.Printf("Error connecting to blockchain.info: %s", err)
-		}
-		doc.Find("#total_received").Each(func(i int, s *goquery.Selection) {
-			complete := s.Find("span").Text()
-			// final := s.Find("span").Text()
-			if complete == "0 BTC" {
-				fmt.Printf("no prior transactions by pk %x\n", pktodisplay)
-			} else {
-				fmt.Printf("possible wallet found in pk %x, added to needles.txt\n", pktodisplay)
-				file, err := os.OpenFile("needles.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+				// caddr, _ := btcutil.NewAddressPubKey(public.SerializeCompressed(), &chaincfg.MainNetParams)
+				uaddr, _ := btcutil.NewAddressPubKey(public.SerializeUncompressed(), &chaincfg.MainNetParams)
+
+				transactions, err := node.BlockchainAddressGetHistory(uaddr.EncodeAddress())
 				if err != nil {
-					fmt.Printf("Error pushing data to file: %s", err)
+					fmt.Printf("error with address %s: %s", uaddr.EncodeAddress(), err)
 				}
-				if _, err := file.WriteString(uaddr.EncodeAddress() + " " + string(pktodisplay) + "\n"); err != nil {
-					log.Fatal(err)
+				if len(transactions) == 0 {
+					fmt.Printf("address %s has no history..\n", uaddr.EncodeAddress())
+				} else {
+					fmt.Printf("address %s has use history!!!!!!!!! (private key %x)\n", uaddr.EncodeAddress(), pktodisplay)
+					file, err := os.OpenFile("needles.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+					if err != nil {
+						fmt.Printf("Error pushing data to file: %s", err)
+						os.Exit(666)
+					}
+					if _, err := file.WriteString(uaddr.EncodeAddress() + " " + string(pktodisplay) + "\n"); err != nil {
+						fmt.Printf("Error pushing data to file: %s", err)
+						os.Exit(666)
+					}
 				}
 			}
-			// fmt.Printf("%x %34s %34s\n", pktodisplay, uaddr.EncodeAddress(), caddr.EncodeAddress())
-		})
+		}(line, rng, maxprivatekeys)
 	}
+	wg.Wait()
 }
